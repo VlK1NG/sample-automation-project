12c8f8cbe2defb41dc3a212fbcfabad452b4621c
Dmytro Davydov <fs.davydov@gmail.com>
2022-10-17 20:08:41 +0300

    Implemented TestNG framework Added AgeChecker class with age access check Added AgeCheckerTest class with testAgedUserCanPlay and testThatTooYoungUsersCanNotPlay

--- src/main/java/net/absoft/mortalwombat/Main.java
+++ src/main/java/net/absoft/mortalwombat/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package net.absoft.mortalwombat;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -0,0 +1,18 @@
+package net.absoft.mortalwombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+    @Test
+    public void testAgedUserCanPlay() {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can't play game");
+    }
+
+    @Test
+    public void testThatTooYoungUsersCanNotPlay() {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(17), "Aged user can't play game");
+    }
+}
